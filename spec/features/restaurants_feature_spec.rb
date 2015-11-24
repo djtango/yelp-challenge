e6d85278e7a8b883449cd7a7fa833ee4d1e2ab66
Deon Tan <deonjtan@gmail.com>
2015-11-24 13:12:30 +0000

    second feature test - adding restaurant added.

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -1,4 +1,3 @@
-
 require 'rails_helper'
 feature ' restaurants' do
   context 'no restaurants have been added' do
@@ -8,7 +7,6 @@ feature ' restaurants' do
       expect(page).to have_link 'Add a restaurant'
     end
   end
-
   context 'restaurants have been added' do
     before do
       Restaurants.create(name: 'KFC')
@@ -20,5 +18,4 @@ feature ' restaurants' do
       expect(page).not_to have_content('No restaurants yet')
     end
   end
-
 end
