371cd5fb4cac3c1f4e5280aaa4ef5a595c4ae4c4
octavianrotari <octavianrotari@gmail.com>
2015-11-26 17:47:58 +0000

    v1 released

--- yelp_clone/spec/models/review_spec.rb
+++ yelp_clone/spec/models/review_spec.rb
@@ -2,4 +2,8 @@ require 'rails_helper'
 
 describe Review, type: :model do
   it {should belong_to :restaurant}
+  it 'it is invalid if the raiting is more than 5' do
+    review = Review.new(rating:10)
+    expect(review).to have(1).error_on(:rating)
+  end
 end
